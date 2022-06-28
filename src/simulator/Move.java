8421158c9dc56789cd54c8bd45290713bcf1b4ae
LittleTealeaf <thomaskwashnak@gmail.com>
2022-06-27 22:12:30 -0400

    stuffz

--- src/simulator/Move.java
+++ src/simulator/Move.java
@@ -1,15 +1,16 @@
 package simulator;
 
 import java.util.Arrays;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public enum Permutation {
+public enum Move {
     R(new Point[][]{
             {
-
+                Point.WOB,Point.OYB,Point.YRB,Point.RWB
             }
     }),
+    R2(R,Transform.TWO),
+    RP(R,Transform.PRIME),
     U,
     L,
     D,
@@ -18,16 +19,16 @@ public enum Permutation {
 
     private final Point[][] permutations;
 
-    Permutation(Permutation permutation, Transform transform) {
+    Move(Move move, Transform transform) {
         this.permutations = switch (transform) {
-            case PRIME -> Arrays.stream(permutation.permutations).parallel().map(perm -> {
+            case PRIME -> Arrays.stream(move.permutations).parallel().map(perm -> {
                 Point[] a = new Point[perm.length];
                 for (int i = 0; i < a.length; i++) {
                     a[i] = perm[perm.length - 1 - i];
                 }
                 return a;
             }).toList().toArray(new Point[0][]);
-            case TWO -> Arrays.stream(permutation.permutations).parallel().flatMap(perm -> {
+            case TWO -> Arrays.stream(move.permutations).parallel().flatMap(perm -> {
                 Point[][] a = new Point[2][perm.length / 2];
                 for (int i = 0; i < perm.length; i++) {
                     a[i % 2][i / 2] = perm[i];
@@ -37,7 +38,7 @@ public enum Permutation {
         };
     }
 
-    Permutation(Point[]... permutations) {
+    Move(Point[]... permutations) {
         this.permutations = permutations;
     }
 
